43940833f9b99b3d4dbd6bbffe38c676c61fa2c0
Mike Brown <101634661+MDBU1@users.noreply.github.com>
2023-11-21 12:32:18 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -13,12 +13,13 @@ setup(
     name='sars_cov2_signals',
     version='2.2',
     packages=['modules/PandasToPowerpoint', 'AutoSignals', "requirements.txt"],
-    py_modules=["main.py"],
+    # py_modules=["main.py"],
     url='https://gitlab.phe.gov.uk/gpha/horizon-scanning/sars_cov2_signals_automation',
     license='',
     author='Mike Brown',
     author_email='michael.d.brown@phe.gov.uk',
-    description='This package is intended for the manual and or automation running of signal review analysis which serves function within Horizon Scanning (GPHA) bi-weekly reporting and meetings',
+    description='This package is intended for the manual and or automation running of signal review analysis which '
+                'serves function within Horizon Scanning (GPHA) bi-weekly reporting and meetings',
     long_description=read("README.md"),
     classifiers=["Development Status :: 5 - Production/Stable"],
 )
