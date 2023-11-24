51a1059acc9af7f6f828e69005196ad42c56974f
Mike Brown <101634661+MDBU1@users.noreply.github.com>
2023-11-24 14:35:09 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
 from modules.run_signals import ClassSignalRun
-
+import IPython
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
